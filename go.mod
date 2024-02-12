7773acf1167c188107f328cc96512e863c0db391
nemo-omen <nemo.omen@gmail.com>
2024-02-12 16:16:33 -0600

    Update to server-side highlighting

--- go.mod
+++ go.mod
@@ -12,12 +12,15 @@ require (
 
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/alecthomas/chroma/v2 v2.12.0 // indirect
+	github.com/dlclark/regexp2 v1.10.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/yuin/goldmark-highlighting/v2 v2.0.0-20230729083705-37449abec8cc // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
--- service/markdown.go
+++ service/markdown.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"fmt"
 
+	chromahtml "github.com/alecthomas/chroma/v2/formatters/html"
 	"github.com/yuin/goldmark"
+	highlighting "github.com/yuin/goldmark-highlighting/v2"
 	"github.com/yuin/goldmark/extension"
 	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/renderer/html"
@@ -16,7 +18,16 @@ func (s MarkdownService) ParseMarkdownContent(content []byte) (string, error) {
 	var buf bytes.Buffer
 
 	md := goldmark.New(
-		goldmark.WithExtensions(extension.GFM),
+		goldmark.WithExtensions(
+			extension.GFM,
+			highlighting.NewHighlighting(
+				highlighting.WithStyle("catppuccin-mocha"),
+				highlighting.WithFormatOptions(
+					chromahtml.WithLineNumbers(true),
+					// chromahtml.WithClasses(true),
+				),
+			),
+		),
 		goldmark.WithParserOptions(
 			parser.WithAutoHeadingID(),
 		),
